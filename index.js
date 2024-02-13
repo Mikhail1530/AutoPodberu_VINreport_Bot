723dbefcd63c4c794301ee343924877bc5f6dad5
Mikhail1530 <michael.nfnr@gmail.com>
2024-02-13 18:21:45 +0400

    update Bot

--- index.js
+++ index.js
@@ -1,53 +1,38 @@
 const TelegramApi = require('node-telegram-bot-api')
-const token = '6838248687:AAE1ohr2ciZL26u1RtLsRqH9p0cd2EBmNdI'
 const axios = require('axios')
+const token = '6838248687:AAE1ohr2ciZL26u1RtLsRqH9p0cd2EBmNdI'
 const bot = new TelegramApi(token, {polling: true})
+const fsPromises = require('fs').promises
+const fs = require('fs')
+const sequelize = require('./db')
+const Client = require('./models')
+const {logger} = require("sequelize/lib/utils/logger");
 
 
-const tokenVin = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZpcm9ubWVudCI6InRlc3QiLCJ1c2VyIjp7ImlkIjoyMDg1MTEsImVtYWlsIjoiYXV0b3BvZGJlcnUxKzFAZ21haWwuY29tIn0sInZlbmRvciI6eyJpZCI6MjczLCJzdGF0dXMiOiJhY3RpdmUiLCJpcCI6WyIxNzIuMjAuMTAuMyIsIjU0Ljg2LjUwLjEzOSIsIjE4NS4xMTUuNC4xNDciLCIxODUuMTE1LjUuMjgiLCI1LjE4OC4xMjkuMjM2Il19LCJpYXQiOjE3MDYwMTI1NzQsImV4cCI6MTcwODYwNDU3NH0.D5hOhF4CUOcMlyE4meRRPggfnZpKejKgDcHrlAWM6e4'
 const tokenPayment = '381764678:TEST:77012'
 const instance = axios.create({
     baseURL: "https://www.clearvin.com/rest/vendor/",
-    responseType: "arraybuffer",
-    headers: {
-        Authorization: `Bearer ${tokenVin}`,
-    },
 });
 
-// variables to dataBase--------
-let listUsersWithCheckPoints = {}
-let listChecksPerMonth = 0
-let listChecksAll = 0
-let listChecksAllFree = []
-let listChatIdUsers = ['2133980094', '2133980092', '2133980194', '2133980192', '2133280194']
-
 // static variables---------
-const danila_ID = 342056317
-// const danila_ID = 2133980094
+// const danila_ID = 342056317
+// const danila_ID = 342056311
 const telegramChannelId = '-1001815620648'
+const greetings = `<b>Приветствую тебя!</b> 👋 \n\nЯ - твой надежный помощник в мире автомобилей, также я умею узнавать информацию об авто по VIN-номеру! \nБуду присылать тебе экспертные советы и интересные новости из мира авто. 🚗💨 \n\n<b><i><u>Подпишись на наш канал</u></i></b>, чтобы быть в курсе всех последних анонсов и эксклюзивных предложений + получи одну <b>бесплатную проверку</b> для авто. 💼🛣️\nt.me/autopodberu`
+const danila_ID = 2133980094
 
-// variables rewritable--------
 let success = 0
 let notSend = 0
-const resetFunction = () => {
-    success = 0
-    notSend = 0
-}
-const resetVarPerMonth = () => {
-    setInterval(() => {
-        listChecksPerMonth = 0
-    }, 30 * 24 * 60 * 60)
-}
-
 
 const KEYBOARD = {
     reply_markup: JSON.stringify({
         keyboard: [
-            ['💳 Купить проверки ($)', '⚖ Проверить остаток проверок'],
-            ['✅ VIN', '👍 Получить бесплатную проверку'],
+            ['💳 Купить проверки ($)', '⚖ Узнать остаток проверок'],
+            ['✅ VIN', '👍 Бесплатная проверка'],
         ],
-        resize_keyboard: true
-    })
+        resize_keyboard: true,
+    }),
+    parse_mode: 'HTML'
 }
 const KEYBOARD_ADMIN = {
     reply_markup: JSON.stringify({
@@ -56,7 +41,8 @@ const KEYBOARD_ADMIN = {
             ['🤙 Статистика последних отправленных']
         ],
         resize_keyboard: true
-    })
+    }),
+    parse_mode: 'HTML'
 }
 const checksOptions = {
     reply_markup: JSON.stringify({
@@ -65,78 +51,119 @@ const checksOptions = {
             [{text: 'Купить 3 проверки (14$)', callback_data: 'ThreeCheckVIN'}],
             [{text: 'Купить 5 проверок (20$)', callback_data: 'FiveCheckVIN'}]
         ]
-    })
+    }),
+    parse_mode: "HTML"
 }
-const start = () => {
-    // reset variable (listChecksPerMonth) every 30 days
-    resetVarPerMonth()
 
-    bot.setMyCommands([
+
+const start = async () => {
+
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+    } catch (e) {
+        console.log('Connect data failed', e)
+    }
+
+    await bot.setMyCommands([
         {command: '/start', description: 'Запустить бота'},
     ])
+
     bot.onText(/(.+)/, async (msg, match) => {
+        const chatId = msg.chat.id
 
-        if (match[0] == '/start' && msg.from.id === danila_ID) {
-            return bot.sendMessage(msg.chat.id, 'Привет Danila 👋🏻', KEYBOARD_ADMIN)
-        }
 
-        if (match[0] == '/start') {
-            !listChatIdUsers.includes(msg.chat.id) ? listChatIdUsers.push(msg.chat.id) : ''
-            return bot.sendMessage(msg.chat.id, 'Привет кукушкин 👋🏻, залетай на покупки. Подпишись туда-сюда и будеш один раз проверку делать', KEYBOARD)
-        }
-        if (match[0] == '👍 Получить бесплатную проверку') {
-            try {
-                bot.getChatMember(telegramChannelId, msg.chat.id).then(res => {
-                    if (listChecksAllFree.includes(msg.chat.id)) {
-                        return bot.sendMessage(msg.chat.id, 'Вы уже получили бесплатную проверку за подписку на канал', {parse_mode: 'HTML'})
+        try {
+            if (match[0] === '/start' && chatId === danila_ID) {
+                return bot.sendMessage(chatId, 'Здравствуй, курчявенький Данилкин 👋🏻', KEYBOARD_ADMIN)
+            }
+            if (match[0] === '/start') {
+                try {
+                    await Client.create({chatId})
+                    return bot.sendMessage(chatId, greetings, KEYBOARD)
+                } catch (e) {
+                    const user = await Client.findOne({where: {chatId: chatId}})
+                    return user && bot.sendMessage(chatId, 'Бот уже запущен', KEYBOARD)
+                }
+            }
+
+            if (match[0] === '👍 Бесплатная проверка') {
+                try {
+                    const res = await bot.getChatMember(telegramChannelId, chatId)
+                    const validFreeCheck = await Client.findOne({where: {chatId: chatId}})
+
+                    if (res.status === 'member' && !validFreeCheck.freeCheck) {
+                        try {
+                            await Client.increment('checks', {by: 1, where: {chatId: chatId}})
+                            await Client.update({freeCheck: true}, {where: {chatId: chatId}})
+                            return bot.sendMessage(chatId, 'Вы подписаны на канал <b>AutoPodberu</b> и за это мы дарим вам одну <b>бесплатную проверку</b> по VIN номеру для авто', {parse_mode: 'HTML'})
+                        } catch (e) {
+                            console.log(e, 'Ошибка получения проверок за подписку')
+                        }
+                    }
+                    if (res.status === 'member' && validFreeCheck.freeCheck) {
+                        return bot.sendMessage(chatId, 'Вы уже получили бесплатную проверку за подписку на наш телеграм-канал <i><b>AutoPodberu</b></i>.\n\nНо вы всегда можете приобрести проверки нажав на кнопку <b>💳 Купить проверки ($)</b>', {parse_mode: 'HTML'})
                     }
                     if (res.status === 'left') {
-                        return bot.sendMessage(msg.chat.id, "Вы не подписаны на канал AutoPodberu, чтобы получить бесплатну проверку по VIN номеру необходимо подписаться на канал <b>t.me/autopodberu</b>", {parse_mode: 'HTML'})
-                    } else {
-                        listChecksAllFree.push(msg.chat.id)
-                        listUsersWithCheckPoints[msg.from.id] ? listUsersWithCheckPoints[msg.from.id] += 1 : listUsersWithCheckPoints[msg.from.id] = 1
-                        return bot.sendMessage(msg.chat.id, 'Вы подписаны на канал <b>AutoPodberu</b> и за это мы дарим вам одну бесплатную проверку по VIN номеру для авто', {parse_mode: 'HTML'})
+                        return bot.sendMessage(chatId, "Вы не подписаны на канал <i><b>AutoPodberu</b></i>, чтобы получить бесплатну проверку по VIN номеру необходимо подписаться на наш телеграм-канал <b>t.me/autopodberu</b>\n\nПосле подписки снова нажмите на кнопку <b>'👍 Получить бесплатную проверку'</b>\n\n", {parse_mode: 'HTML'})
                     }
-                })
-            } catch (e) {
-                return bot.sendMessage(msg.chat.id, 'Ошибка')
+                } catch (e) {
+                    return bot.sendMessage(chatId, 'Ошибка')
+                }
             }
-            return
-        }
 
 
-        if (match[0] == '💳 Купить проверки ($)') {
-            return bot.sendMessage(msg.chat.id, 'Выберете нужное количество', checksOptions)
-        }
+            if (match[0] === '💳 Купить проверки ($)') {
+                return bot.sendMessage(chatId, '<i>Выберете нужное количество 🎳</i>', checksOptions)
+            }
 
-        if (match[0] == '✅ VIN' && Object.keys(listUsersWithCheckPoints).includes(`${msg.from.id}`)) {
-            return bot.sendMessage(msg.chat.id, 'Введите <b>VIN</b> авто (<i>17 символов</i>)', {parse_mode: 'HTML'})
-        }
+            if (match[0] === '✅ VIN') {
+                // const res = await Client.findAll()
+                // console.log(res.map(u=>u["dataValues"]['chatId']))
+                return bot.sendMessage(chatId, 'Если у вас есть доступные проверки, то просто вбейте в строку ввода <b><i>VIN номер</i></b> (<i>17 символов</i>) и получите подробную информацию об автомобиле в <i>PDF-файле</i> 📂\n\nОстаток проверок можно узнать нажав на соответствующую кнопку ⚖', {parse_mode: 'HTML'})
+            }
 
-        if (match[0].length === 17 && Object.keys(listUsersWithCheckPoints).includes(`${msg.from.id}`)) {
-            //  запрос за проверкой по VIN - сделать try catch
-            await bot.sendMessage(msg.chat.id, 'Запрос сделан')
-            // if request is valid -> listChecksAll += 1 and listChecksPerMonth += 1
-            listUsersWithCheckPoints[msg.from.id] > 0 ? listUsersWithCheckPoints[msg.from.id] -= 1 : await bot.sendMessage(msg.chat.id, 'У вас не осталось проверок', KEYBOARD)
-            listChecksAll += 1
-            listChecksPerMonth += 1
-        }
+            if (match[0].length === 17) {
+                const user = await Client.findOne({where: {chatId: chatId}})
+                if (user.checks === 0) {
+                    return bot.sendMessage(chatId, 'К сожалению, у вас не осталось проверок по VIN номеру.\n\nНо вы всегда можете их приобрести нажав на кнопку <b>💳 Купить проверки ($)</b>', {parse_mode: 'HTML'})
+                }
+                if (user.checks > 0) {
+                    await bot.sendMessage(chatId, 'Запрос займет немного времени, ожидайте')
+                    const url = `report?vin=${msg.text}&format=pdf&reportTemplate=2021`
 
-        if (match[0] == '⚖ Проверить остаток проверок') {
-            const userChecks = listUsersWithCheckPoints[msg.from.id] ? listUsersWithCheckPoints[msg.from.id] : 0
-            return bot.sendMessage(msg.chat.id, `У вас осталось проверок: ${userChecks}`)
-        }
 
-        if (match[0] == '💌 Рассылка для контактов' && msg.from.id === danila_ID) {
-            return bot.sendMessage(msg.chat.id, `\n<b>1) Чтобы отправить картинку и текст:</b> Просто отправляй фото и описание к ней.\n<b>2) Чтобы отправить просто текст:</b> Поставить звездочку (*) перед сообщением. (<i>пример:</i> *куку галоши) `, {parse_mode: 'HTML'})
-        }
-        if (match[0] == '🤙 Статистика последних отправленных' && msg.from.id === danila_ID) {
-            await bot.sendMessage(msg.from.id, `\n<i>Сообщения получено:</i> ${success}\n<i>Сообщений не доставлено:</i> ${notSend}`, {parse_mode: 'HTML'})
-        }
-        if (match[0] == '☎ Статистика проверок' && msg.from.id === danila_ID) {
-            return bot.sendMessage(msg.chat.id, `\n<b>Всего проверок:</b> <i>${listChecksAll}</i>\n<b>Проверок за месяц:</b> <i>${listChecksPerMonth}</i>\n<b>Активированные проверки за подписку:</b> <i>${listChecksAllFree.length}</i>`, {parse_mode: 'HTML'})
-        } else {
-            msg.chat.id !== danila_ID ? await bot.sendMessage(msg.chat.id, 'Неизвестная команда, выберете команду в меню кнопок') : ''
+                }
+
+
+            }
+
+            if (match[0] === '⚖ Узнать остаток проверок') {
+                const check = await Client.findOne({where: {chatId: chatId}})
+                return bot.sendMessage(chatId, `У вас осталось проверок: <b>${check.checks}</b>`, {parse_mode: 'HTML'})
+            }
+
+            // Block options Danila
+            if (match[0] === '💌 Рассылка для контактов' && chatId === danila_ID) {
+                return bot.sendMessage(chatId, `\n<b>1) Чтобы разослать картинку и текст подписчикам:</b> <i>Просто отправляй фото и описание к ней.</i>\n\n<b>2) Чтобы отправить текст без картинки:</b> <i>Необходимо поставить две звездочки (**) перед сообщением. (например: **Привет человеки)</i>`, {parse_mode: 'HTML'})
+            }
+            if (match[0] === '🤙 Статистика последних отправленных' && chatId === danila_ID) {
+                await bot.sendMessage(chatId, `\n<i>Сообщения получено:</i> ${success}\n<i>Сообщений не доставлено:</i> ${notSend}`, {parse_mode: 'HTML'})
+            }
+            if (match[0] === '☎ Статистика проверок' && chatId === danila_ID) {
+                const res = await Client.findAll()
+                const freeChecks = await Client.findAll({where: {freeCheck: true}})
+                const allChecks = res.map(c => c['dataValues']['checks']).reduce((acc, cur)=>{
+                    acc+=cur
+                    return acc
+                }, 0)
+                // const freeChecks = res.map(c => c['dataValues']['freeChecks']).filter(c=>c === true)
+                return bot.sendMessage(chatId, `\n<b>Всего проверок куплено:</b> <i>${allChecks - freeChecks.length}</i>\n<b>Активированные проверки за подписку:</b> <i>${freeChecks.length}</i>`, {parse_mode: 'HTML'})
+            } else {
+                chatId !== danila_ID ? await bot.sendMessage(chatId, 'Неизвестная команда, выберете доступные опции в меню кнопок') : ''
+            }
+        } catch (e) {
+            await bot.sendMessage(chatId, 'Something crashed on the server')
         }
     })
 
@@ -145,28 +172,37 @@ const start = () => {
     bot.on('photo', async msg => {
         const chatId = msg.chat.id
         if (chatId === danila_ID) {
-            resetFunction()
-            listChatIdUsers.map(async (id, i) => {
+            success = 0
+            notSend = 0
+            const res = await Client.findAll()
+            const listIdUsers = res.map(u => u["dataValues"]['chatId'])
+
+            listIdUsers.map(async (id, i) => {
                 try {
                     await bot.sendPhoto(id, msg.photo[1].file_id, {caption: msg.caption}).then(res => success += 1)
                 } catch (e) {
                     notSend += 1
                 }
-                listChatIdUsers.length - 1 === i ? await bot.sendMessage(chatId, 'Сообщения отправлены') : ''
+                listIdUsers.length - 1 === i ? await bot.sendMessage(chatId, 'Сообщения отправлены') : ''
             })
+        } else {
+            return bot.sendMessage(chatId, 'Неизвестная команда, выберете доступные опции в меню кнопок')
         }
     })
     bot.on('text', async msg => {
         const chatId = msg.chat.id
-        if (chatId === danila_ID && msg.text.slice(0, 1) === '*') {
-            resetFunction()
-            listChatIdUsers.map(async (id, i) => {
+        if (chatId === danila_ID && msg.text.slice(0, 2) === '**') {
+            success = 0
+            notSend = 0
+            const res = await Client.findAll()
+            const listIdUsers = res.map(u => u["dataValues"]['chatId'])
+            listIdUsers.map(async (id, i) => {
                 try {
-                    await bot.sendMessage(id, msg.text.slice(1)).then(res => success += 1)
+                    await bot.sendMessage(id, msg.text.slice(2)).then(res => success += 1)
                 } catch (e) {
                     notSend += 1
                 }
-                listChatIdUsers.length - 1 === i ? await bot.sendMessage(chatId, 'Сообщения отправлены') : ''
+                listIdUsers.length - 1 === i ? await bot.sendMessage(chatId, 'Сообщения отправлены') : ''
             })
         }
     })
@@ -176,7 +212,6 @@ const start = () => {
     bot.on('callback_query', async (msg) => {
         const data = msg.data
         const chatId = msg.message.chat.id
-
         if (data === 'OneCheckVIN') {
             await bot.sendInvoice(
                 chatId,
@@ -194,14 +229,14 @@ const start = () => {
                 try {
                     return bot.answerPreCheckoutQuery(ctx.id, true)
                 } catch (error) {
-                    return error ? bot.sendMessage(ctx.id, 'Ошибка платежа') : ''
+                    return error ? bot.sendMessage(ctx.id, 'Ошибка платежной системы') : ''
                 }
             })
             await bot.on('successful_payment', async ctx => {
                 try {
                     if (ctx.successful_payment.invoice_payload === msg.id) {
                         await bot.sendMessage(ctx.chat.id, `Спасибо за оплату`)
-                        await listUsersWithCheckPoints[msg.from.id] ? listUsersWithCheckPoints[msg.from.id] += 1 : listUsersWithCheckPoints[msg.from.id] = 1
+                        await Client.increment('checks', {by: 1, where: {chatId: chatId}}).then(res=>console.log(res, '1')).catch(e=>console.log(e, '2'))
                         return bot.sendMessage(ctx.chat.id, 'Вы приобрели одну проверку по VIN номеру')
                     }
                 } catch (error) {
@@ -228,14 +263,14 @@ const start = () => {
                 try {
                     return bot.answerPreCheckoutQuery(ctx.id, true)
                 } catch (error) {
-                    return bot.sendMessage(ctx.id, 'Ошибка платежа')
+                    return bot.sendMessage(ctx.id, 'Ошибка платежной системы')
                 }
             })
             await bot.on('successful_payment', async ctx => {
                 try {
                     if (ctx.successful_payment.invoice_payload === msg.id) {
                         await bot.sendMessage(ctx.chat.id, `Спасибо за оплату`)
-                        await listUsersWithCheckPoints[msg.from.id] ? listUsersWithCheckPoints[msg.from.id] += 3 : listUsersWithCheckPoints[msg.from.id] = 3
+                        await Client.increment('checks', {by: 3, where: {chatId: chatId}})
                         return bot.sendMessage(chatId, 'Вы приобрели 3 проверки по VIN номеру', KEYBOARD)
                     }
                 } catch (error) {
@@ -262,14 +297,14 @@ const start = () => {
                 try {
                     return bot.answerPreCheckoutQuery(ctx.id, true)
                 } catch (error) {
-                    return bot.sendMessage(ctx.id, 'Ошибка платежа')
+                    return bot.sendMessage(ctx.id, 'Ошибка платежной системы')
                 }
             })
             await bot.on('successful_payment', async ctx => {
                 try {
                     if (ctx.successful_payment.invoice_payload === msg.id) {
                         await bot.sendMessage(ctx.chat.id, `Спасибо за оплату`)
-                        await listUsersWithCheckPoints[msg.from.id] ? listUsersWithCheckPoints[msg.from.id] += 5 : listUsersWithCheckPoints[msg.from.id] = 5
+                        await Client.increment('checks', {by: 5, where: {chatId: chatId}})
                         return bot.sendMessage(chatId, 'Вы приобрели 5 проверок по VIN номеру', KEYBOARD)
                     }
                 } catch (error) {
---
+++ models.js
@@ -0,0 +1,16 @@
+const sequelize = require('./db')
+const {DataTypes} = require('sequelize')
+
+
+
+const Client = sequelize.define('Client', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+    chatId: {type: DataTypes.INTEGER, unique: true},
+    checks: {type: DataTypes.INTEGER, defaultValue: 0},
+    freeCheck: {type: DataTypes.BOOLEAN, defaultValue: false}
+}, {
+    timestamps: false,
+    tableName: 'Client'
+})
+
+module.exports = Client
